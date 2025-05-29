0b79092d0e442f34bdefaf11577802fbf304da08
MastyGit <MastyGit@proton.me>
2025-05-29 17:04:44 +0300

    Add getComputerChoice function
    Gets random choice between rock, paper or scissors

--- js/js.js
+++ js/js.js
@@ -1,1 +1,17 @@
-const hello = "Hello, World";
+//Rock paper scrissors
+//
+//Pseudocode for computer's choice function
+//
+//GET computer's choice
+//	GET random integer between 1 and 3	
+//	IF random integer is 1 return rock
+//	IF random integer is 2 return paper
+//	IF random integer is 3 return scrissor
+//END
+
+function getComputerChoice() {
+    const randomInt = Math.floor((Math.random() * 3) + 1);
+    return randomInt === 1 ? "rock"
+    : randomInt === 2 ? "paper"
+    : "scissors";
+}
